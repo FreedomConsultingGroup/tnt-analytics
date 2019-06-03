6767095708893ac7789aabd265668d7091a854a0
Scott Beall <scottb.21074@gmail.com>
2019-06-03 00:05:51 -0400

    cool

---
+++ python/q4.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from analytic2 import *
+
+setContext()
+es=getEs()
+userid=getUser()
+question4(es,userid)
---
+++ python/q6.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+from analytic2 import *
+
+setContext()
+es=getEs()
+userid=getUser()
+question6(es,userid)
